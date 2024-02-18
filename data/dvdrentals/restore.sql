97a3a3b3f599baf14bccbff360b68820e69471e4
Christopher Wetherill <git@tbmh.org>
2024-02-18 17:05:50 -0500

    test alt path

--- data/dvdrentals/restore.sql
+++ data/dvdrentals/restore.sql
@@ -933,7 +933,7 @@ ALTER TABLE public.staff_list OWNER TO codio;
 
 COPY public.actor (actor_id, first_name, last_name, last_update) FROM stdin;
 \.
-COPY public.actor (actor_id, first_name, last_name, last_update) FROM '$$PATH$$/3057.dat';
+COPY public.actor (actor_id, first_name, last_name, last_update) FROM '/home/codio/workspace/data-1500/data/dvdrentals/3057.dat';
 
 --
 -- Data for Name: address; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -941,7 +941,7 @@ COPY public.actor (actor_id, first_name, last_name, last_update) FROM '$$PATH$$/
 
 COPY public.address (address_id, address, address2, district, city_id, postal_code, phone, last_update) FROM stdin;
 \.
-COPY public.address (address_id, address, address2, district, city_id, postal_code, phone, last_update) FROM '$$PATH$$/3065.dat';
+COPY public.address (address_id, address, address2, district, city_id, postal_code, phone, last_update) FROM '/home/codio/workspace/data-1500/data/dvdrentals/3065.dat';
 
 --
 -- Data for Name: category; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -949,7 +949,7 @@ COPY public.address (address_id, address, address2, district, city_id, postal_co
 
 COPY public.category (category_id, name, last_update) FROM stdin;
 \.
-COPY public.category (category_id, name, last_update) FROM '$$PATH$$/3059.dat';
+COPY public.category (category_id, name, last_update) FROM '/home/codio/workspace/data-1500/data/dvdrentals/3059.dat';
 
 --
 -- Data for Name: city; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -957,7 +957,7 @@ COPY public.category (category_id, name, last_update) FROM '$$PATH$$/3059.dat';
 
 COPY public.city (city_id, city, country_id, last_update) FROM stdin;
 \.
-COPY public.city (city_id, city, country_id, last_update) FROM '$$PATH$$/3067.dat';
+COPY public.city (city_id, city, country_id, last_update) FROM '/home/codio/workspace/data-1500/data/dvdrentals/3067.dat';
 
 --
 -- Data for Name: country; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -965,7 +965,7 @@ COPY public.city (city_id, city, country_id, last_update) FROM '$$PATH$$/3067.da
 
 COPY public.country (country_id, country, last_update) FROM stdin;
 \.
-COPY public.country (country_id, country, last_update) FROM '$$PATH$$/3069.dat';
+COPY public.country (country_id, country, last_update) FROM '/home/codio/workspace/data-1500/data/dvdrentals/3069.dat';
 
 --
 -- Data for Name: customer; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -973,7 +973,7 @@ COPY public.country (country_id, country, last_update) FROM '$$PATH$$/3069.dat';
 
 COPY public.customer (customer_id, store_id, first_name, last_name, email, address_id, activebool, create_date, last_update, active) FROM stdin;
 \.
-COPY public.customer (customer_id, store_id, first_name, last_name, email, address_id, activebool, create_date, last_update, active) FROM '$$PATH$$/3055.dat';
+COPY public.customer (customer_id, store_id, first_name, last_name, email, address_id, activebool, create_date, last_update, active) FROM '/home/codio/workspace/data-1500/data/dvdrentals/3055.dat';
 
 --
 -- Data for Name: film; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -981,7 +981,7 @@ COPY public.customer (customer_id, store_id, first_name, last_name, email, addre
 
 COPY public.film (film_id, title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating, last_update, special_features, fulltext) FROM stdin;
 \.
-COPY public.film (film_id, title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating, last_update, special_features, fulltext) FROM '$$PATH$$/3061.dat';
+COPY public.film (film_id, title, description, release_year, language_id, rental_duration, rental_rate, length, replacement_cost, rating, last_update, special_features, fulltext) FROM '/home/codio/workspace/data-1500/data/dvdrentals/3061.dat';
 
 --
 -- Data for Name: film_actor; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -989,7 +989,7 @@ COPY public.film (film_id, title, description, release_year, language_id, rental
 
 COPY public.film_actor (actor_id, film_id, last_update) FROM stdin;
 \.
-COPY public.film_actor (actor_id, film_id, last_update) FROM '$$PATH$$/3062.dat';
+COPY public.film_actor (actor_id, film_id, last_update) FROM '/home/codio/workspace/data-1500/data/dvdrentals/3062.dat';
 
 --
 -- Data for Name: film_category; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -997,7 +997,7 @@ COPY public.film_actor (actor_id, film_id, last_update) FROM '$$PATH$$/3062.dat'
 
 COPY public.film_category (film_id, category_id, last_update) FROM stdin;
 \.
-COPY public.film_category (film_id, category_id, last_update) FROM '$$PATH$$/3063.dat';
+COPY public.film_category (film_id, category_id, last_update) FROM '/home/codio/workspace/data-1500/data/dvdrentals/3063.dat';
 
 --
 -- Data for Name: inventory; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -1005,7 +1005,7 @@ COPY public.film_category (film_id, category_id, last_update) FROM '$$PATH$$/306
 
 COPY public.inventory (inventory_id, film_id, store_id, last_update) FROM stdin;
 \.
-COPY public.inventory (inventory_id, film_id, store_id, last_update) FROM '$$PATH$$/3071.dat';
+COPY public.inventory (inventory_id, film_id, store_id, last_update) FROM '/home/codio/workspace/data-1500/data/dvdrentals/3071.dat';
 
 --
 -- Data for Name: language; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -1013,7 +1013,7 @@ COPY public.inventory (inventory_id, film_id, store_id, last_update) FROM '$$PAT
 
 COPY public.language (language_id, name, last_update) FROM stdin;
 \.
-COPY public.language (language_id, name, last_update) FROM '$$PATH$$/3073.dat';
+COPY public.language (language_id, name, last_update) FROM '/home/codio/workspace/data-1500/data/dvdrentals/3073.dat';
 
 --
 -- Data for Name: payment; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -1021,7 +1021,7 @@ COPY public.language (language_id, name, last_update) FROM '$$PATH$$/3073.dat';
 
 COPY public.payment (payment_id, customer_id, staff_id, rental_id, amount, payment_date) FROM stdin;
 \.
-COPY public.payment (payment_id, customer_id, staff_id, rental_id, amount, payment_date) FROM '$$PATH$$/3075.dat';
+COPY public.payment (payment_id, customer_id, staff_id, rental_id, amount, payment_date) FROM '/home/codio/workspace/data-1500/data/dvdrentals/3075.dat';
 
 --
 -- Data for Name: rental; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -1029,7 +1029,7 @@ COPY public.payment (payment_id, customer_id, staff_id, rental_id, amount, payme
 
 COPY public.rental (rental_id, rental_date, inventory_id, customer_id, return_date, staff_id, last_update) FROM stdin;
 \.
-COPY public.rental (rental_id, rental_date, inventory_id, customer_id, return_date, staff_id, last_update) FROM '$$PATH$$/3077.dat';
+COPY public.rental (rental_id, rental_date, inventory_id, customer_id, return_date, staff_id, last_update) FROM '/home/codio/workspace/data-1500/data/dvdrentals/3077.dat';
 
 --
 -- Data for Name: staff; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -1037,7 +1037,7 @@ COPY public.rental (rental_id, rental_date, inventory_id, customer_id, return_da
 
 COPY public.staff (staff_id, first_name, last_name, address_id, email, store_id, active, username, password, last_update, picture) FROM stdin;
 \.
-COPY public.staff (staff_id, first_name, last_name, address_id, email, store_id, active, username, password, last_update, picture) FROM '$$PATH$$/3079.dat';
+COPY public.staff (staff_id, first_name, last_name, address_id, email, store_id, active, username, password, last_update, picture) FROM '/home/codio/workspace/data-1500/data/dvdrentals/3079.dat';
 
 --
 -- Data for Name: store; Type: TABLE DATA; Schema: public; Owner: postgres
@@ -1045,7 +1045,7 @@ COPY public.staff (staff_id, first_name, last_name, address_id, email, store_id,
 
 COPY public.store (store_id, manager_staff_id, address_id, last_update) FROM stdin;
 \.
-COPY public.store (store_id, manager_staff_id, address_id, last_update) FROM '$$PATH$$/3081.dat';
+COPY public.store (store_id, manager_staff_id, address_id, last_update) FROM '/home/codio/workspace/data-1500/data/dvdrentals/3081.dat';
 
 --
 -- Name: actor_actor_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
